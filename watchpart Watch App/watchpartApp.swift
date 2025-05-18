6896e10da0422a20efe351a9f176bab6c0b0514f
Your Name <user@example.com>
2025-05-18 23:35:45 +0300

    Initial Commit

---
+++ watchpart Watch App/watchpartApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchpartApp.swift
+//  watchpart Watch App
+//
+//  Created by  Apple on 18.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct watchpart_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchpart Watch AppTests/watchpart_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  watchpart_Watch_AppTests.swift
+//  watchpart Watch AppTests
+//
+//  Created by  Apple on 18.05.2025.
+//
+
+import Testing
+@testable import watchpart_Watch_App
+
+struct watchpart_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
